718b54d10a2644f575991d84fca4ebb1bfa3dae9
Varga György Márk <gyorgy.varga@shiwaforce.com>
2023-11-03 13:28:53 +0100

    init project

---
+++ README.md
@@ -0,0 +1,10 @@
+# Vue Router
+
+To start this project you need to run:
+
+```npm install```
+
+and then
+
+```npm run dev```
+
---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav class="bg-gray-800 p-4">
+    <router-link class="text-white hover:text-gray-300 mr-4" to="/">Home</router-link> |
+    <router-link class="text-white hover:text-gray-300 ml-4" to="/about">About</router-link>
+  </nav>
+  <main class="p-4">
+    <router-view />
+  </main>
+</template>
